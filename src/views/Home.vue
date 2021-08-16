aae1cea907f19a36b88dfcd0cff9affc30ffc3dc
Jianfeng He <jianfeng.he@oliverwyman.com>
2021-08-16 23:19:22 +0800

    finish the vue3 modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <div class="modal-content">
+        <h1>This is a modal header</h1>
+        <p>This is a modal message</p>
+      </div>
+    </Modal>
+    <button @click="toggleModal">Toggle Modal</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Modal from '@/components/Modal.vue'
+import { ref } from '@vue/reactivity'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Modal
+  },
+  setup() {
+    const modalActive = ref(false)
+
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value
+    }
+
+    return { modalActive, toggleModal }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background-color: rgba(0, 176, 234, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+    h1,
+    p {
+      margin-bottom: 16px;
+    }
+
+    h1 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+}
+</style>
